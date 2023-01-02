7b48c564d5191a26963b7bff6cc550a9ea9fed50
joan-ramirez <joan_montilla@hotmail.com>
2023-01-01 21:09:14 -0400

    first commit

---
+++ HandlingDB.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\HandlingDB;
+
+class DB
+{
+    public function export(string $entity, array $where = [], array $select = ['*'])
+    {
+        $data = $entity::where($where);
+        $file = fopen(public_path('contacts.csv'), 'a');
+
+        $offset = 0;
+        $limit = 50;
+
+        for ($i = 0; $i < $data->count();) {
+
+            $data = $data->select($select)
+                ->offset($offset++ * $limit)
+                ->limit($limit)
+                ->get();
+
+            foreach ($data as $line)
+                fputcsv($file, $line->getAttributes());
+
+            $i += $limit;
+        }
+
+        if (fclose($file))
+            return "exported!";
+    }
+
+    public function joinsExport()
+    {
+    }
+
+    static function import(string $entity, array $where = [], string $format = null)
+    {
+    }
+}
